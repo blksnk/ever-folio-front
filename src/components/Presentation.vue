e7cf971311941f71f3dee1f12dd80e3c1a413f14
blksnk <art.jnveigel@gmail.com>
2020-05-27 12:00:59 +0200

    :construction: old layout presentation and work sections

---
+++ src/components/Presentation.vue
@@ -0,0 +1,70 @@
+<template>
+  <Card
+    id="pres"
+    :corners="{
+      BottomRight: true,
+    }"
+  >
+    <div id="left">
+      <h1>Ever Noël</h1>
+      <h2>ux/ui designer</h2>
+    </div>
+
+    <div id="right">
+      <p>Veniam quis nostrud ut ex elit ex aute eiusmod ut in.</p>
+      <p>
+        Veniam quis nostrud ut ex elit ex aute eiusmod ut in. Ut do consectetur
+        consequat eu cillum velit deserunt dolor et ex adipisicing ullamco sit
+        deserunt ex aliquip eiusmod non id culpa consequat ut irure sed
+        reprehenderit dolor in commodo ullamco ut excepteur id fugiat ad ea
+        aute.
+      </p>
+    </div>
+  </Card>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import Card from '@/components/ui/Card.vue';
+
+@Component({
+  components: {
+    Card,
+  },
+})
+export default class Presentation extends Vue {}
+</script>
+
+<style scoped lang="scss">
+#pres {
+  min-height: 80vh;
+  display: grid;
+  grid-template-columns: 6fr 4fr;
+  grid-template-rows: 1fr auto;
+  padding-top: $spacing * 3;
+
+  #left {
+    margin-left: $spacing;
+
+    h1 {
+      grid-column: 1 / 1;
+      grid-row: 1 / 1;
+      margin: 0 !important;
+    }
+
+    h2 {
+      color: $color_tertiary;
+    }
+  }
+
+  #right {
+    margin-top: $spacing;
+    grid-column: 2 / 2;
+    grid-row: 1 / 3;
+
+    p {
+      margin-bottom: $spacing;
+    }
+  }
+}
+</style>
---
+++ src/components/Work.vue
@@ -0,0 +1,67 @@
+<template>
+  <section id="work">
+    <Card id="left">
+      <h2>project title</h2>
+
+      <div id="project_info">
+        <span>client:</span>
+        <span>date:</span>
+        <span>category:</span>
+        <span>description:</span>
+        <span>client:</span>
+      </div>
+    </Card>
+    <Card
+      id="right"
+      :corners="{
+        TopLeft: true,
+        BottomLeft: true,
+      }"
+      :color="colorblack"
+      cornerRadius="2rem"
+    >
+      <ImageRounded
+        src="https://images.adsttc.com/media/images/5def/9ad6/3312/fd16/7900/03cf/large_jpg/TIFF-STROM-QC-1.jpg?1575983774"
+      />
+    </Card>
+  </section>
+</template>
+
+<script>
+import Card from '@/components/ui/Card.vue';
+import ImageRounded from '@/components/ui/ImageRound.vue';
+import { colorblack } from '@/stylesheets/variables.scss';
+
+export default {
+  name: 'Work',
+  components: {
+    Card,
+    ImageRounded,
+  },
+  data() {
+    return {
+      colorblack,
+    };
+  },
+};
+</script>
+
+<style scoped lang="scss">
+#work {
+  display: grid;
+  grid-template-columns: 6fr 4fr;
+  grid-template-rows: auto;
+  min-height: 80vh;
+  background-color: $color_white;
+
+  #left {
+    grid-column: 1 / 1;
+    margin-left: $spacing;
+  }
+
+  #right {
+    grid-column: 2 / 2;
+    padding-right: 0;
+  }
+}
+</style>
