63a81e0643b14cb447cf987bfbde823ae9152ba8
blksnk <art.jnveigel@gmail.com>
2020-05-27 12:08:54 +0200

    :construction: old layout card wrapper

---
+++ src/views/Wrapper.vue
@@ -0,0 +1,21 @@
+<template>
+  <main>
+    <slot></slot>
+  </main>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Wrapper extends Vue {}
+</script>
+
+<style lang="scss">
+main {
+  width: 100%;
+  min-height: 100vh;
+  padding-right: 2 * $spacing;
+  background-color: $color_black;
+}
+</style>
