c9a175f37b46008d964dfe93ba1291974dbd1e55
blksnk <art.jnveigel@gmail.com>
2020-05-27 12:04:31 +0200

    :sparkles: rounded corners image component

---
+++ src/components/ui/ImageRound.vue
@@ -0,0 +1,20 @@
+<template>
+  <img class="img" :src="src" alt="" />
+</template>
+
+<script>
+export default {
+  name: 'ImageRound',
+  props: ['src'],
+};
+</script>
+
+<style scoped lang="scss">
+.img {
+  overflow: hidden;
+  border-radius: $radius;
+  object-fit: cover;
+  width: 100%;
+  height: 100%;
+}
+</style>
