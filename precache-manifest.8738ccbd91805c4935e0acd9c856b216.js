b206fb2aab10e49892e02e9cf1e0ac4052b73c7e
blksnk <art.jnveigel@gmail.com>
2020-05-27 12:36:43 +0200

    Deploy

---
+++ precache-manifest.8738ccbd91805c4935e0acd9c856b216.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6a6fbbe36c6161fef22",
+    "url": "/ever-folio-front/css/about.4f9affb1.css"
+  },
+  {
+    "revision": "d50092c32514dbf46b61",
+    "url": "/ever-folio-front/css/app.dd0c9658.css"
+  },
+  {
+    "revision": "d4495d6904523fb43dc9931b0c3d49d2",
+    "url": "/ever-folio-front/fonts/NeueMachina-Black.d4495d69.otf"
+  },
+  {
+    "revision": "0d936fb5d5aa7259ba0321e5cd111990",
+    "url": "/ever-folio-front/fonts/NeueMachina-Light.0d936fb5.otf"
+  },
+  {
+    "revision": "5f810f9f188465a4bfbb712d3c956273",
+    "url": "/ever-folio-front/fonts/NeueMachina-Regular.5f810f9f.otf"
+  },
+  {
+    "revision": "5f37678fd44f12a7978d560203cec98a",
+    "url": "/ever-folio-front/fonts/NeueMachina-Ultrabold.5f37678f.otf"
+  },
+  {
+    "revision": "6d4fdc7cf4cb23a94858e912fe3ac3c9",
+    "url": "/ever-folio-front/fonts/NeueMachina-Ultralight.6d4fdc7c.otf"
+  },
+  {
+    "revision": "589aa09d74a9e875a86c1e042950e83d",
+    "url": "/ever-folio-front/fonts/Pilowlava-Regular.589aa09d.woff2"
+  },
+  {
+    "revision": "dbb353b1c53447295b516f49aefc2135",
+    "url": "/ever-folio-front/fonts/Pilowlava-Regular.dbb353b1.woff"
+  },
+  {
+    "revision": "e99677416a51ce326aaada691a4e3919",
+    "url": "/ever-folio-front/fonts/Pilowlava-Regular.e9967741.eot"
+  },
+  {
+    "revision": "ebf23bd251bc7403f2fb5a5efed1cb29",
+    "url": "/ever-folio-front/fonts/Pilowlava-Regular.ebf23bd2.otf"
+  },
+  {
+    "revision": "8f74dfbcd7cda107b2c5a8fa8ca659fd",
+    "url": "/ever-folio-front/fonts/canopee.8f74dfbc.woff"
+  },
+  {
+    "revision": "1e9f7e945e6295afff594657deccd1e5",
+    "url": "/ever-folio-front/fonts/love.1e9f7e94.woff"
+  },
+  {
+    "revision": "b7bed5c731cde3d5c002e21469fab218",
+    "url": "/ever-folio-front/fonts/voyage.b7bed5c7.woff"
+  },
+  {
+    "revision": "b050a2c99bbfcef32b13f5302b61c475",
+    "url": "/ever-folio-front/index.html"
+  },
+  {
+    "revision": "b6a6fbbe36c6161fef22",
+    "url": "/ever-folio-front/js/about.9d316594.js"
+  },
+  {
+    "revision": "d50092c32514dbf46b61",
+    "url": "/ever-folio-front/js/app.1f12faee.js"
+  },
+  {
+    "revision": "2dbee77cd73d6d731e6b",
+    "url": "/ever-folio-front/js/chunk-vendors.26332194.js"
+  },
+  {
+    "revision": "1ecee0c343b1bc0443f2e6eba14ed6ff",
+    "url": "/ever-folio-front/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/ever-folio-front/robots.txt"
+  }
+]);
